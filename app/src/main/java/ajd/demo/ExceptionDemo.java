d270f82d3c4902ef58709c873651cd464b56df1d
zj <zj@webturing.com>
2019-01-03 11:53:59 +0800

    24点算法讲解代码：逆波兰式求值和判断合法性，java集合类，洗牌算法，随机化搜索

---
+++ app/src/main/java/ajd/demo/ExceptionDemo.java
@@ -0,0 +1,27 @@
+package ajd.demo;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            System.out.println(1 / 0);
+        } catch (ArithmeticException e) {
+            System.out.println("I catch you!");
+        }
+        System.out.println("hello exception");
+        int a[] = {1, 2, 3};
+        try {
+            System.out.println(a[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            int b[] = new int[a.length * 2];
+            for (int i = 0; i < a.length; i++) {
+                b[i] = a[i];
+            }
+            a = b;
+            System.out.println(a[3]);
+
+        }
+        a[4] = -7;
+        System.out.println(Math.sqrt(a[4]));
+
+    }
+}
